45b4cba725cf044d0292c4069d88e41cd8e6ca6a
Adriano Reis <adriano_silvareis@hotmail.com>
2023-04-29 15:42:34 -0300

    refactor(ClientService): compose baseUrl

--- src/app/pages/clients/services/clients.service.ts
+++ src/app/pages/clients/services/clients.service.ts
@@ -8,26 +8,27 @@ import { ClientModel } from '@pages/clients/model/Clients.model';
 })
 export class ClientsService {
   public ROOT_URL = environment.baseUrl
+  private baseUrl = `${this.ROOT_URL}/clients`
 
   constructor(private httpClient: HttpClient) { }
 
   loadClients() {
-    return this.httpClient.get<ClientModel[]>(`${this.ROOT_URL}/clients`);
+    return this.httpClient.get<ClientModel[]>(this.baseUrl);
   }
 
   getClientById(clientId: string) {
-    return this.httpClient.get<ClientModel>(`${this.ROOT_URL}/clients/${clientId}`);
+    return this.httpClient.get<ClientModel>(`${this.baseUrl}/${clientId}`);
   }
 
   addClients(client: ClientModel) {
-    return this.httpClient.post<ClientModel>(`${this.ROOT_URL}/clients`, client);
+    return this.httpClient.post<ClientModel>(this.baseUrl, client);
   }
 
   editClients(client: ClientModel) {
-    return this.httpClient.put<ClientModel>(`${this.ROOT_URL}/clients/${client.id}`, client);
+    return this.httpClient.put<ClientModel>(`${this.baseUrl}/${client.id}`, client);
   }
 
   deleteClients(client: ClientModel) {
-    return this.httpClient.delete<ClientModel>(`${this.ROOT_URL}/clients/${client.id}`);
+    return this.httpClient.delete<ClientModel>(`${this.baseUrl}/${client.id}`);
   }
 }
