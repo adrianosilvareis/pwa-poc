26708ffb6af0765ff3debe0d807fb87a27ed611d
Adriano Reis <adriano_silvareis@hotmail.com>
2023-06-30 16:30:50 -0300

    refactor: join props of input in only one component

--- src/app/shared/components/form/currency-form/currency-form.component.ts
+++ src/app/shared/components/form/currency-form/currency-form.component.ts
@@ -1,25 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { InputFieldPropsComponent } from '@components/form/input-field-props.component';
 
 @Component({
   selector: 'app-currency-form',
   templateUrl: './currency-form.component.html',
   styleUrls: ['./currency-form.component.scss']
 })
-export class CurrencyFormComponent implements OnInit{
-  @Input() inputKey = '';
-  @Input() group!: FormGroup;
-  @Input() label = '';
-  @Input() placeholder = 'R$ 0,00';
-  @Input() clearable!: boolean;
+export class CurrencyFormComponent extends InputFieldPropsComponent {
 
-  control!: FormControl;
-
-  ngOnInit() {
-    this.control = this.group.get(this.inputKey) as FormControl
-  }
-
-  clear() {
-    this.control.setValue(null)
-  }
 }
--- src/app/shared/components/form/datepick-form/datepick-form.component.ts
+++ src/app/shared/components/form/datepick-form/datepick-form.component.ts
@@ -1,25 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { InputFieldPropsComponent } from '@components/form/input-field-props.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-datepick-form',
   templateUrl: './datepick-form.component.html',
   styleUrls: ['./datepick-form.component.scss']
 })
-export class DatepickFormComponent implements OnInit {
-  @Input() inputKey = '';
-  @Input() group!: FormGroup;
-  @Input() label = '';
-  @Input() placeholder = 'Choose a Data';
-  @Input() clearable!: boolean;
-
-  control = new FormControl('');
-
-  ngOnInit() {
-    this.control = this.group.get(this.inputKey) as FormControl
-  }
-
-  clear() {
-    this.control.setValue(null)
+export class DatepickFormComponent extends InputFieldPropsComponent {
+  constructor() {
+    super()
+    this.placeholder = 'Choose a Data'
   }
 }
