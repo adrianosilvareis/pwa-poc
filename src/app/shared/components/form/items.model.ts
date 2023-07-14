ca408c4ba5712a7d602e5d5c26e43197e65d2160
Adriano Reis <adriano_silvareis@hotmail.com>
2023-07-14 00:19:38 -0300

    feat: created multi-select-form component

--- src/app/shared/components/form/items.model.ts
+++ src/app/shared/components/form/items.model.ts
@@ -3,6 +3,7 @@ import { FormControl } from "@angular/forms";
 export enum FieldType {
   input = 'input',
   autocomplete = 'autocomplete',
+  multiselect = 'multiselect',
   date = 'date',
   YesNo = 'YesNo',
   currency = 'currency',
