b57e32c0dc355c15a9c6b3276448a44842d4af7b
Adriano Reis <adriano_silvareis@hotmail.com>
2023-07-28 00:39:58 -0300

    chore: added .env to the project

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:5000',
+  patterns: {
+    locale: 'en-US',
+    dateFormat: 'dd/MM/yyyy',
+    currencyFormat: 'BRL',
+  },
+  JWT: {
+    access_token_key: 'access_token_2',
+    refresh_token_key: 'refresh_token',
+  }
+};
