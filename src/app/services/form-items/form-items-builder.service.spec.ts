116fdccbcb7c0a00c0f9f806d39cf05f99a0bd2e
Adriano Reis <adriano_silvareis@hotmail.com>
2023-07-28 21:41:59 -0300

    feat: make disabled option to form-items-builder

--- src/app/services/form-items/form-items-builder.service.spec.ts
+++ src/app/services/form-items/form-items-builder.service.spec.ts
@@ -4,56 +4,34 @@ import { FormItemsBuilderService } from './form-items-builder.service';
 import { FormControl, Validators } from '@angular/forms';
 
 describe('FormItemsBuilderService', () => {
-  const items = [
-    {
-      colspan: 1,
-      name: "name",
-      placeholder: "Name",
-      label: "Name",
-      value: [''],
-      type: FieldType.input,
-    },
-    {
-      colspan: 1,
-      name: "description",
-      placeholder: "Description",
-      label: "Description",
-      value: [''],
-      type: FieldType.input,
-    },
+  const items: FormItems[] = [
+    { colspan: 1, name: "name", placeholder: "Name", label: "Name", disabled: false, value: new FormControl(''), type: FieldType.input },
+    { colspan: 1, name: "description", placeholder: "Description", label: "Description", disabled: false, value: new FormControl(''), type: FieldType.input },
+    { colspan: 1, name: "email", placeholder: "Email", label: "Email", disabled: false, value: new FormControl('', [Validators.required, Validators.email]), type: FieldType.input },
+    { colspan: 1, name: "total", placeholder: "Total", label: "Total", disabled: true, value: new FormControl(100), type: FieldType.input },
   ]
-  it('should be created a FormItems', () => {
-    // given
-    const builder = new FormItemsBuilderService();
-
-    // when
-    const itens = builder
-                    .addItem({ name: 'name', value: '' })
-                    .addItem({ name: 'description', value: '' })
-                    .build();
-
-    // then
-    expect(itens).toEqual(items);
-  });
 
   it('should be created a FormItems with validations', () => {
     // given
-    const validations = [Validators.required];
     const builder = new FormItemsBuilderService();
-    const validateNameItem = items.at(0);
-    const validateDescriptionItem = { ...items.at(1), value: new FormControl('', validations) };
 
     // when
     const buildedItems = builder
                             .addItem({ name: 'name', value: '' })
-                            .addItem({ name: 'description', value: '' }).addValidations(validations)
+                            .addItem({ name: 'description', value: '' }).addValidations([Validators.required])
+                            .addItem({ name: 'email', value: '' }).addValidations([Validators.required, Validators.email])
+                            .addItem({ name: 'total', value: 100 }).disabled()
                             .build();
 
-    const builderNameItem = buildedItems.at(0) as FormItems;
-    const builderDescriptionItem = buildedItems.at(1) as FormItems;
-
-    // then
-    expect(JSON.stringify(builderNameItem)).toEqual(JSON.stringify(validateNameItem));
-    expect(JSON.stringify(builderDescriptionItem)).toEqual(JSON.stringify(validateDescriptionItem));
+    buildedItems.forEach((item, index) => {
+      expect(item.colspan).toEqual(items.at(index)?.colspan);
+      expect(item.label).toEqual(items.at(index)?.label);
+      expect(item.name).toEqual(items.at(index)?.name);
+      expect(item.placeholder).toEqual(items.at(index)?.placeholder);
+      expect(item.type).toEqual(items.at(index)?.type);
+      expect(item.value.value).toEqual(items.at(index)?.value.value);
+      expect(item.value.hasValidator).toEqual(items.at(index)?.value.hasValidator);
+      expect(item.value.disabled).toBe(items.at(index)?.disabled);
+    })
   });
 });
