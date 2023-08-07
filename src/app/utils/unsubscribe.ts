724fbed9fdf78bbccfd0488126c659c20ae54bc9
Adriano Reis <adriano_silvareis@hotmail.com>
2023-08-06 22:52:19 -0300

    feat: added destroyed subject to use in takeUntil

--- src/app/utils/unsubscribe.ts
+++ src/app/utils/unsubscribe.ts
@@ -1,12 +1,15 @@
 import { Injectable, OnDestroy } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Subject, Subscription } from "rxjs";
 
 @Injectable()
 export class Unsubscribe implements OnDestroy {
 
   protected subs = new Subscription();
+  protected destroyed$ = new Subject();
 
   ngOnDestroy(): void {
+    this.destroyed$.next(null);
+    this.destroyed$.complete();
     this.subs.unsubscribe()
   }
 }
