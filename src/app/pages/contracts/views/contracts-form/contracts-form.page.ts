2a629927408a987803c803946661fb1f18dc230b
Adriano Reis <adriano_silvareis@hotmail.com>
2023-08-06 23:11:25 -0300

    feat: added price, suggestedValue and discount relationship rules

--- src/app/pages/contracts/views/contracts-form/contracts-form.page.ts
+++ src/app/pages/contracts/views/contracts-form/contracts-form.page.ts
@@ -11,10 +11,17 @@ import { isContractLoading, selectedContract } from '@root/app/pages/contracts/s
 import { FormItemsBuilder } from '@root/app/services/form-items/form-items.builder';
 import { Unsubscribe } from '@root/app/utils/unsubscribe';
 import { ContractsModel } from '@pages/contracts/model/contracts.models';
-import { ContractsData } from "../../model/contracts.data";
+import { ContractsData } from "@pages/contracts/model/contracts.data";
 import { servicesPageActions } from '@root/app/pages/company-services/store/company-services.actions';
-import { combineLatest } from 'rxjs';
+import { Observable, combineLatest, takeUntil } from 'rxjs';
 import { CompanyServicesModel } from '@root/app/pages/company-services/model/company-services.model';
+import { ContractsFormUpdateService } from '@pages/contracts/services/contracts-form-update/contracts-form-update.service';
+import { selectActiveClients } from '@root/app/pages/clients/store/clients.selectors';
+import { ClientModel } from '@root/app/pages/clients/model/client.model';
+import { OptionsType } from '@root/app/shared/components/form/protocols/options-type';
+import { clientsPageActions } from '@root/app/pages/clients/store/clients.actions';
+
+type ContractsOptions = { client: OptionsType[], services: OptionsType[] };
 
 @Component({
   template: `
@@ -25,8 +32,7 @@ import { CompanyServicesModel } from '@root/app/pages/company-services/model/com
       role="form"
       (save)="saveContract($event)"
       (cancel)="cancel()"
-      (valueChange)="onChange($event)"
-      ref="reference"
+      (mounted)="onMounted($event)"
     ></app-form>
   `
 })
@@ -36,8 +42,10 @@ export class ContractsFormPage extends Unsubscribe implements OnInit {
   data!: FormItems[];
 
   services!: CompanyServicesModel[];
+  clients!: ClientModel[];
 
   servicesOptions$ = this.store.select(selectActiveServices);
+  clientsOptions$ = this.store.select(selectActiveClients);
   contract$ = this.store.select(selectedContract);
 
   constructor(
@@ -45,22 +53,55 @@ export class ContractsFormPage extends Unsubscribe implements OnInit {
     private router: Router,
     private builder: FormItemsBuilder,
     private store: Store<AppState>,
+    private contractRules: ContractsFormUpdateService,
   ) { super(); }
 
-  onChange({ value, form }: { value: ContractsModel, form: FormGroup }) {
-    if (value.services === null) {
-      form.get('suggestedValue')?.setValue(0);
-      return;
+  onMounted({ form }: { form: FormGroup }) {
+    const statusFlag = {
+      discount: false,
+      price: false,
     }
-    const total = value.services.reduce((acc, service) => {
-      acc += Number(this.services.find(({ id }) => id === (service as unknown as string))?.value);
-      return acc;
-    },0)
-    form.get('suggestedValue')?.setValue(total);
+    this.contractRules.updateSuggestedValue(this.services, form);
+    this.onServicesUpdate(form);
+    this.onPriceOrSuggestedChanges(form, statusFlag);
+    this.onDiscountUpdate(form, statusFlag);
+  }
+
+  private onDiscountUpdate(form: FormGroup, statusFlag: { discount: boolean; price: boolean; }) {
+    const discount$ = form.get('discount')?.valueChanges as Observable<number>;
+    discount$.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      if (statusFlag.price) {
+        statusFlag.price = !statusFlag.price;
+        return;
+      }
+      statusFlag.discount = true;
+      this.contractRules.updatePriceValue(form);
+    });
+  }
+
+  private onServicesUpdate(form: FormGroup) {
+    const services$ = form.get('services')?.valueChanges as Observable<number>;
+    services$.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      this.contractRules.updateSuggestedValue(this.services, form);
+    });
+  }
+
+  private onPriceOrSuggestedChanges(form: FormGroup, statusFlag: { discount: boolean; price: boolean; }) {
+    const suggestedValue$ = form.get('suggestedValue')?.valueChanges as Observable<number>;
+    const price$ = form.get('price')?.valueChanges as Observable<number>;
+    combineLatest([suggestedValue$, price$]).pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      if (statusFlag.discount) {
+        statusFlag.discount = !statusFlag.discount;
+        return;
+      }
+      statusFlag.price = true;
+      this.contractRules.updateDiscount(form);
+    });
   }
 
   ngOnInit() {
     this.store.dispatch(servicesPageActions.loadServices());
+    this.store.dispatch(clientsPageActions.loadClients());
     this.subs.add(this.fillId());
     this.subs.add(this.generateForm());
   }
@@ -81,6 +122,7 @@ export class ContractsFormPage extends Unsubscribe implements OnInit {
   private backToList() {
     return this.store
       .select(isContractLoading)
+      .pipe(takeUntil(this.destroyed$))
       .subscribe(isLoading => { if(!isLoading) { this.router.navigate(['/contracts']); } });
   }
 
@@ -97,37 +139,53 @@ export class ContractsFormPage extends Unsubscribe implements OnInit {
       this.store.dispatch(contractsPageActions.selectContract({ contract: null }));
     }
 
-    return combineLatest([this.contract$, this.servicesOptions$]).subscribe(([item, services]) => {
-      this.services = services;
-      this.contract = item;
-      if (item) {
-        this.data = this.buildData(services, item);
-      } else {
-        this.data = this.buildData(services)
-      }
-    });
+    return combineLatest([this.contract$, this.servicesOptions$, this.clientsOptions$])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(([item, services, clients]) => {
+        if (!services || !clients) return;
+        this.services = services;
+        this.clients = clients;
+        this.contract = item;
+
+        const options = {
+          client: this.clients.map(({ id, name }) => ({ value: id, label: name })),
+          services: this.services.map(({ id, title }) => ({ value: id, label: title }))
+        }
+        if (item) {
+          this.data = this.buildData(options, item);
+        } else {
+          this.data = this.buildData(options)
+        }
+      });
   }
 
-  private buildData(servicesOptions: CompanyServicesModel[], item?: ContractsModel) {
+  private buildData(options: ContractsOptions, item?: ContractsModel) {
     return this.builder
-      .addItem({ name: 'startDate', value: item?.startDate, clearable: true, type: FieldType.date })
+      .addItem({ name: 'client', label: 'Cliente', value: item?.client, clearable: true, type: FieldType.autocomplete })
+        .addOptions(options.client).addValidations([Validators.required])
+      .addItem({ name: 'startDate', label: 'Data de Inicio', value: item?.startDate ?? new Date(), clearable: true, type: FieldType.date })
         .addValidations([Validators.required])
-      .addItem({ name: 'endDate', value: item?.endDate, clearable: true, type: FieldType.date })
-      .addItem({ name: 'services', value: item?.services.map(({ id }) => id), clearable: true, type: FieldType.multiselect })
-        .addOptions(servicesOptions.map(service => ({ value: service.id, label: service.title })))
-      .addItem({ name: 'price', value: item?.price, clearable: true, type: FieldType.currency })
-      .addItem({ name: 'renewable', value: item?.renewable, clearable: true, type: FieldType.YesNo })
-      .addItem({ name: 'suggestedValue', value: item?.suggestedValue, type: FieldType.currency }).disabled()
+      .addItem({ name: 'endDate', label: 'Data de Fim', value: item?.endDate, clearable: true, type: FieldType.date })
+      .addItem({ name: 'services', label: 'Serviços', value: item?.services.map(({ id }) => id), clearable: true, type: FieldType.multiselect })
+        .addOptions(options.services).addValidations([Validators.required])
+      .addItem({ name: 'price', label: 'Preço', value: item?.price ?? 0, clearable: true, type: FieldType.currency })
+      .addItem({ name: 'suggestedValue', label: 'Preço Sugerido', value: item?.suggestedValue ?? 0, type: FieldType.currency }).disabled()
+      .addItem({ name: 'discount', suffix: '%',  label: 'Desconto', value: item?.discount ?? 0, type: FieldType.input }).addValidations([Validators.min(0), Validators.max(80)])
+      .addItem({ name: 'renewable', label: 'Renovável', value: item?.renewable ?? true, clearable: true, type: FieldType.YesNo })
       .build();
   }
 
   private sanitize(formContract: ContractsData):ContractsModel {
+    const suggestedValue = this.contractRules.calcTotalSuggested(this.services, formContract.services)
     return {
       id: this.contract?.id,
+      client: formContract.client,
       startDate: formContract.startDate,
       endDate: formContract.endDate,
       renewable:formContract.renewable,
       price: formContract.price,
+      suggestedValue: formContract.suggestedValue ?? suggestedValue,
+      discount: formContract.discount,
       services: this.getServices(formContract.services),
       isActive: this.contract?.isActive ?? true
     }
