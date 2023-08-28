9dfeff489285738ddb95612a869b56b3d290377b
Adriano Reis <adriano_silvareis@hotmail.com>
2023-08-28 17:29:45 -0300

    feat: added clientName to contract list

--- src/app/pages/contracts/store/contracts.selectors.ts
+++ src/app/pages/contracts/store/contracts.selectors.ts
@@ -1,6 +1,7 @@
 import { createSelector } from "@ngrx/store";
 import { AppState } from "@root/app/app-state";
 import { ContractsState } from "./contracts.reducer";
+import { selectClients } from "@pages/clients/store/clients.selectors";
 
 export const selectContractState = (state: Partial<AppState>) => state.contract as ContractsState;
 
@@ -10,3 +11,12 @@ export const selectedContract = createSelector(selectContractState, (state: Cont
 export const selectedContractId = createSelector(selectContractState, (state: ContractsState) => state.selectedContractId);
 export const selectActiveContracts = createSelector(selectContractState, (state: ContractsState) => state.contracts.filter(({isActive}) => isActive));
 export const selectContracts = createSelector(selectContractState, (state: ContractsState) => state.contracts);
+export const selectContractsList = createSelector(selectContracts, selectClients, (contracts, clients) => {
+  return contracts.map(contract => {
+    const client = clients.find(client => client.id === contract.client);
+    return {
+      ...contract,
+      clientName: client?.name || ''
+    }
+  });
+});
